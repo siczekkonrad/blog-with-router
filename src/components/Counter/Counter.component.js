590e632fd64cd3bf6c2567f776587084dc6b814d
Konrad Siczek <konradd.siczek@gmail.com>
2019-04-20 02:35:15 +0200

    Counter component and date in footer

---
+++ src/components/Counter/Counter.component.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react'
+
+class Counter extends Component {
+    state = {
+        counter: 0
+    }
+    incrementCounter = () => {
+        this.setState(prevState => ({counter: prevState.counter + 1}))
+    }
+    decrementCounter = () => {
+        this.setState(prevState => ({counter: prevState.counter - 1 }))
+    }
+    resetCounter = () => {
+        this.setState({counter: 0})
+    }
+    render() {
+        return(
+            <div className='counter-box'>
+                <span>Stan: {this.state.counter}</span>
+                <button onClick={this.incrementCounter}>+</button>
+                <button onClick={this.decrementCounter}>-</button>
+                <button onClick={this.resetCounter}>RESET</button>
+            </div>
+        )
+    }
+}
+
+export default Counter
--- src/components/Footer/Footer.component.js
+++ src/components/Footer/Footer.component.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 const Footer = () => {
+   
     return (
         <footer className='footer'>
-            &copy; - 2019
+            &copy; - {new Date().getFullYear()}
         </footer>
     )
 }
